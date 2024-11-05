9646ad8121bc6cbbcdb494b4850e453803eba37e
Miron <olimpijada72@gmail.com>
2024-11-05 19:19:11 +0100

    Inital commit

---
+++ src/controller/Controller.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Autor;
+import model.Knjiga;
+import model.Zanr;
+
+/**
+ *
+ * @author user
+ */
+public class Controller {
+    private List<Knjiga> listaKnjiga = new ArrayList<>();
+    private List<Autor> listaAutora = new ArrayList<>();
+
+    
+    private static Controller instance;
+    
+    public static Controller getInstance(){
+        if(instance == null){
+            instance = new Controller();
+        }
+        
+        return instance;
+    }
+    
+    
+    private Controller() {
+        Autor autor1 = new Autor("Ivo", "Andric", 1892, "Biografjia ive andrica" );
+        Autor autor2 = new Autor("Danilo", "Kis", 1900, "Biografjia Kisa" );
+        Autor autor3 = new Autor("Mesa", "Selimovic", 2000, "Biografjia Mese" );
+        
+
+        Knjiga knjiga1 = new Knjiga("Na Drini Cuprija", autor1, "1234968", 1945, Zanr.ROMAN);
+        Knjiga knjiga2 = new Knjiga("Enciklopedija mrtvih", autor2, "1234968", 1975, Zanr.NAUCNA_FANTASTIKA);
+        Knjiga knjiga3 = new Knjiga("Tvrdjava", autor3, "1234968", 1995, Zanr.POEZIJA);
+
+        
+        listaKnjiga.add(knjiga1);
+        listaKnjiga.add(knjiga2);
+        listaKnjiga.add(knjiga3);
+        
+    
+        listaAutora.add(autor1);
+        listaAutora.add(autor2);
+        listaAutora.add(autor3); 
+    }
+
+    public List<Knjiga> getListaKnjiga() {
+        return listaKnjiga;
+    }
+
+    public void setListaKnjiga(List<Knjiga> listaKnjiga) {
+        this.listaKnjiga = listaKnjiga;
+    }
+
+    public List<Autor> getListaAutora() {
+        return listaAutora;
+    }
+
+    public void setListaAutora(List<Autor> listaAutora) {
+        this.listaAutora = listaAutora;
+    }
+    
+    public void obrisiKnjigu(int selektovaniRed){
+        
+        listaKnjiga.remove(selektovaniRed);
+    }
+
+    public void dodajKnjigu(Knjiga novaKnjiga) {
+        listaKnjiga.add(novaKnjiga);
+        System.out.println("KNJIGA JE DODATA");
+        
+    }
+    
+}
---
+++ src/model/Knjiga.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author user
+ */
+public class Knjiga {
+    private String naslov;
+    private Autor autor;
+    private String ISBN;
+    private int godinaIzdanja;
+    private Zanr zanr;
+
+    public Knjiga() {
+    }
+
+    public String getNaslov() {
+        return naslov;
+    }
+
+    public void setNaslov(String naslov) {
+        this.naslov = naslov;
+    }
+
+    
+    public Knjiga(String naslov, Autor autor, String ISBN, int godinaIzdanja, Zanr zanr) {
+        this.naslov = naslov;
+        this.autor = autor;
+        this.ISBN = ISBN;
+        this.godinaIzdanja = godinaIzdanja;
+        this.zanr = zanr;
+    }
+    
+    
+    
+     
+    
+            
+            
+            
+            
+        
+            
+            
+            
+            
+    
+    
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public int getGodinaIzdanja() {
+        return godinaIzdanja;
+    }
+
+    public Zanr getZanr() {
+        return zanr;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public void setGodinaIzdanja(int godinaIzdanja) {
+        this.godinaIzdanja = godinaIzdanja;
+    }
+
+    public void setZanr(Zanr zanr) {
+        this.zanr = zanr;
+    }
+
+    @Override
+    public String toString() {
+        return "Knjiga{" + "naslov=" + naslov + ", autor=" + autor + ", ISBN=" + ISBN + ", godinaIzdanja=" + godinaIzdanja + ", zanr=" + zanr + '}';
+    }
+
+    
+     
+    
+}
